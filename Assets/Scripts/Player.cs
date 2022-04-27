58aef578eae3c9c521de14d60a20f4e1ad74a558
PabloGoldman <pablo.goldmanarca@gmail.com>
2022-04-27 20:39:53 -0300

    Updated player values.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -21,7 +21,7 @@ public class Player : MonoBehaviour
     private int score = 0;
     public int GetScore() => score;
 
-    private float angleTreshold = 45;
+    private float angleTreshold = 42;
 
     private void Awake()
     {
